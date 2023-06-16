1a779a17d2583a6bf3b56ee10bf597e40b72baa2
DimitriBoldon <dimitri@tiksdd.com>
2023-06-15 20:20:45 -0500

    edited code

--- assets/script.js
+++ assets/script.js
@@ -34,3 +34,39 @@ function onPlayerReady(event) {
 function onPlayerStateChange(event) {
 // We can perform additional operations here if needed
 }
+
+
+const field = document.querySelector('textarea');
+const backUp = field.getAttribute('placeholder')
+const btn = document.querySelector('button is-info');
+// const clear = document.getElementById('clear')
+const submit = document.querySelector('#submitbutton')
+
+const comments_arr = [];
+
+const display_comments = () => {
+  let list = '<ul>';
+   comments_arr.forEach(comment => {
+    list += `<li>${comment}</li>`;
+  })
+  list += '</ul>';
+  comments.innerHTML = list;
+}
+
+clear.onclick = function(event){
+  event.preventDefault();
+  
+   comments_arr.length = 0;
+  
+  display_comments();
+}
+
+submit.onclick = function(event){
+    event.preventDefault();
+    const content = field.value;
+    if(content.length > 0){ 
+      comments_arr.push(content);
+      display_comments(); 
+      field.value = '';
+    }
+}
